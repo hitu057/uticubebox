3e0b92234b668ecc0123831f8e9d7d85f87ec850
Hitesh Chaudhari <hitesh.chaudhari@bajajfinserv.in>
2024-06-20 23:51:06 +0530

    added console

--- api/controller/attendance.js
+++ api/controller/attendance.js
@@ -368,6 +368,8 @@ async function compareFaces(image1Path, image2Path) {
         const options = new faceapi.TinyFaceDetectorOptions({ inputSize: 224, scoreThreshold: 0.5 })
         const detections1 = await faceapi.detectAllFaces(img1, options).withFaceLandmarks().withFaceDescriptors()
         const detections2 = await faceapi.detectAllFaces(img2, options).withFaceLandmarks().withFaceDescriptors()
+        console.log(detections1)
+        console.log(detections2)
         if (!detections1?.length || !detections2?.length) {
             console.log('1')
             return false
